50831750cf0414ece53f3122585fa3a4bcdf32f9
Dilshan Ushara <dilshanushara@Dilshans-MacBook-Pro.local>
2021-08-10 12:01:51 +0530

    Updated Prescription UI

--- Front-end/health-care/src/app/prescription/create/create.component.ts
+++ Front-end/health-care/src/app/prescription/create/create.component.ts
@@ -5,6 +5,7 @@ import { MedicineService } from 'src/app/medicine/medicine.service';
 import { Medicine } from 'src/app/medicine/model/medicine.model';
 import { patient } from 'src/app/patient/model/patient.model';
 import { PatientService } from 'src/app/patient/patient.service';
+import { AddedMedicine } from '../model/added_medicne.model';
 import { Prescription } from '../model/prescription.model';
 import { PrescriptionService } from '../prescription.service';
 
@@ -19,6 +20,8 @@ export class CreateComponent implements OnInit {
 
   medicines:Medicine[]=[];
   patients:patient[]=[];
+  AddedMedicine:AddedMedicine[]=[];
+  MedicineIDS:string[]=[];
   prescription_medicines:Medicine[]=[];
   ngOnInit(): void {
     this.getMedicine();
@@ -46,22 +49,58 @@ export class CreateComponent implements OnInit {
   }
 
   AddPrescription(prescription:Prescription){
+    console.log("pres comp")
     this.prescriptionservice.addPrescription(prescription).subscribe((data)=>{
       console .log("prescripton component triggerd")
     });
 
   }
 
+  getPatientID(){
+    console.log("patientid selected")
+  }
+
+  myFunc() {
+    var id = ((document.getElementById("MedicineID") as HTMLInputElement).value);
+    this.MedicineIDS.push(id);
+    console.log(this.MedicineIDS);
+
+    let newmedicine:AddedMedicine={
+
+      Dosage :((document.getElementById("Dosage") as HTMLInputElement).value),
+      id : ((document.getElementById("MedicineID") as HTMLInputElement).value),
+      Comment : ((document.getElementById("Comments") as HTMLInputElement).value)
+    }
+//  console.log(newmedicine);
+ this.AddedMedicine.push(newmedicine);
+ 
+ console.log(this.AddedMedicine)
+ 
+    // var Name= ((document.getElementById("MedicineName") as HTMLInputElement).value);
+
+    // console.log(Name);
+}
+
   onSubmit(form :NgForm){
+    if( form.valid){
     let newprescription: Prescription = {
       id:form.value.id,
       PatientID:form.value.PatientID,
-      Subject:form.value.subject,
-      Comment :form.value.comment,
-      MedicineIDS:form.value.medicineIDS
-
+      Subject:form.value.Subject,
+      Comment :form.value.Comment,
+      MedicineIDS:this.MedicineIDS,
+    
       }
       this.AddPrescription(newprescription);
+  }
+}
+
+
+  Remove(id:string){
+    let index = this.AddedMedicine.findIndex(x=>x.id ==id);
+    this.AddedMedicine.splice(index,1);
+    // let index2=this.MedicineIDS.findIndex(x=>);
+    // this.MedicineIDS.splice(index,1)
   }
 
 }
--- Front-end/health-care/src/app/prescription/list/list.component.ts
+++ Front-end/health-care/src/app/prescription/list/list.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { Prescription } from '../model/prescription.model';
+import { PrescriptionService } from '../prescription.service';
 
 @Component({
   selector: 'app-list',
@@ -7,9 +10,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListComponent implements OnInit {
 
-  constructor() { }
+  constructor(private prescriptionservice:PrescriptionService) { }
 
   ngOnInit(): void {
+  }
+
+  AddPrescription(prescription:Prescription){
+    console.log("pres comp")
+    this.prescriptionservice.addPrescription(prescription).subscribe((data)=>{
+      console .log("prescripton component triggerd")
+    });
+  }
+  onSubmit(form :NgForm){
+    let newprescription: Prescription = {
+      id:form.value.id,
+      PatientID:form.value.PatientID,
+      Subject:form.value.Subject,
+      Comment :form.value.Comment,
+      MedicineIDS:form.value.MedicineIDS
+
+      }
+      this.AddPrescription(newprescription);
   }
 
 }
